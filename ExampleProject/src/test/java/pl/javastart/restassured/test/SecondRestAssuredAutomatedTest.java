66cf9419957e97325d27b6fefe8c96761c64bc27
Bartek Dudek <bartlomiej.dudek94@gmail.com>
2025-09-26 07:58:32 +0200

    Zadanie 5.1

--- ExampleProject/src/test/java/pl/javastart/restassured/test/SecondRestAssuredAutomatedTest.java
+++ ExampleProject/src/test/java/pl/javastart/restassured/test/SecondRestAssuredAutomatedTest.java
@@ -1,6 +1,8 @@
 package pl.javastart.restassured.test;
 
 
+import io.restassured.response.Response;
+import io.restassured.response.ValidatableResponse;
 import org.testng.annotations.Test;
 
 import static io.restassured.RestAssured.given;
@@ -10,6 +12,13 @@ public class SecondRestAssuredAutomatedTest {
 
     @Test
     public void givenExistingPetIdWhenGetPetThenReturnPetTest() {
-        given().when().get("https://swaggerpetstore.przyklady.javastart.pl/v2/pet/1").then().statusCode(200);
+                given()
+                    .log().uri().log().method()
+                .when()
+                    .get("https://swaggerpetstore.przyklady.javastart.pl/v2/pet/1")
+                .then()
+                    .log().all().statusCode(200);
+
+
     }
 }
