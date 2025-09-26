871b1418bd0c6645de5ec6065ad18c1ffd76e626
Bartek Dudek <bartlomiej.dudek94@gmail.com>
2025-09-26 08:17:43 +0200

    Zadanie 5.2

---
+++ ExampleProject/src/test/java/pl/javastart/restassured/test/tasks/UserCreationTests.java
@@ -0,0 +1,27 @@
+package pl.javastart.restassured.test.tasks;
+
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class UserCreationTests {
+
+    @Test
+    public void givenCorrectUserDataWhenCreateUserThenUserIsCreatedTest() {
+        given().log().all().contentType("application/json").body(
+        """
+          {
+           "id": 445,
+           "username": "firstuser",
+           "firstName": "Krzysztof",
+           "lastName": "Kowalski",
+           "email": "krzysztof@test.com",
+           "password": "password",
+           "phone": "+123456789",
+           "userStatus": 1
+         }
+         """)
+                .when().post("https://swaggerpetstore.przyklady.javastart.pl/v2/user")
+                .then().log().all().statusCode(200);
+    }
+}
