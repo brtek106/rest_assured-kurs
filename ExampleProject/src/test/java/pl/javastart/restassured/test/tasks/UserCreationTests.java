e6e21b8fe60b458eb95569db782f65c770b4489e
Bartek Dudek <bartlomiej.dudek94@gmail.com>
2025-09-28 19:39:49 +0200

    Zadanie 5.3

--- ExampleProject/src/test/java/pl/javastart/restassured/test/tasks/UserCreationTests.java
+++ ExampleProject/src/test/java/pl/javastart/restassured/test/tasks/UserCreationTests.java
@@ -23,5 +23,9 @@ public class UserCreationTests {
          """)
                 .when().post("https://swaggerpetstore.przyklady.javastart.pl/v2/user")
                 .then().log().all().statusCode(200);
+
+        given().log().all().pathParam("username", "firstuser")
+                .when().get("https://swaggerpetstore.przyklady.javastart.pl/v2/user/{username}")
+                .then().log().all().statusCode(200);
     }
 }
