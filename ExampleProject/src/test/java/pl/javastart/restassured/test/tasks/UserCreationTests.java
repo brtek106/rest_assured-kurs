b923fd6ba89da65570523634d2e13555bda5fd0c
Bartek Dudek <bartlomiej.dudek94@gmail.com>
2025-09-30 09:53:33 +0200

    Zadanie 5.5

--- ExampleProject/src/test/java/pl/javastart/restassured/test/tasks/UserCreationTests.java
+++ ExampleProject/src/test/java/pl/javastart/restassured/test/tasks/UserCreationTests.java
@@ -1,6 +1,7 @@
 package pl.javastart.restassured.test.tasks;
 
 import org.testng.annotations.Test;
+import pl.javastart.main.pojo.user.User;
 
 import static io.restassured.RestAssured.given;
 
@@ -8,23 +9,22 @@ public class UserCreationTests {
 
     @Test
     public void givenCorrectUserDataWhenCreateUserThenUserIsCreatedTest() {
-        given().log().all().contentType("application/json").body(
-        """
-          {
-           "id": 445,
-           "username": "firstuser",
-           "firstName": "Krzysztof",
-           "lastName": "Kowalski",
-           "email": "krzysztof@test.com",
-           "password": "password",
-           "phone": "+123456789",
-           "userStatus": 1
-         }
-         """)
+        User user = new User();
+        user.setId(445);
+        user.setUsername("firstuser");
+        user.setFirstName("Krzysztof");
+        user.setLastName("Kowalski");
+        user.setEmail("krzysztof@test.com");
+        user.setPassword("password");
+        user.setPhone("+123456789");
+        user.setUserStatus(1);
+
+        given().log().all().body(user).contentType("application/json")
                 .when().post("https://swaggerpetstore.przyklady.javastart.pl/v2/user")
                 .then().log().all().statusCode(200);
 
-        given().log().all().pathParam("username", "firstuser")
+        given().log().all().body(user).contentType("application/json")
+                .pathParam("username", user.getUsername())
                 .when().get("https://swaggerpetstore.przyklady.javastart.pl/v2/user/{username}")
                 .then().log().all().statusCode(200);
     }
