5efaa86dec8789afddb5f56f399e732ef7d8e8c2
Bartek <bartlomiej.dudek94@gmail.com>
2025-10-09 11:25:22 +0200

    Zadanie 5.12

--- ExampleProject/src/test/java/pl/javastart/restassured/test/tasks/UserCreationTests.java
+++ ExampleProject/src/test/java/pl/javastart/restassured/test/tasks/UserCreationTests.java
@@ -4,6 +4,7 @@ import org.testng.annotations.Test;
 import pl.javastart.main.pojo.user.User;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
 
 public class UserCreationTests extends TestBase {
 
@@ -17,15 +18,30 @@ public class UserCreationTests extends TestBase {
         user.setEmail("krzysztof@test.com");
         user.setPassword("password");
         user.setPhone("+123456789");
-        user.setUserStatus(1);
+        user.setUserStatus(123);
 
-        given().body(user).contentType("application/json")
+        given().contentType("application/json")
+                .body(user)
                 .when().post("user")
-                .then().statusCode(200);
+                .then()
+                .assertThat().body("code", equalTo(200))
+                .assertThat().body("type", equalTo("unknown"))
+                .assertThat().body("message", equalTo(445))
+                .assertThat().statusCode(200);
 
-        given().body(user).contentType("application/json")
+        given().contentType("application/json")
                 .pathParam("username", user.getUsername())
                 .when().get("user/{username}")
-                .then().statusCode(200);
+                .then()
+                .assertThat().body("id", equalTo(445))
+                .assertThat().body("username", equalTo("firstuser"))
+                .assertThat().body("firstName", equalTo("Krzysztof"))
+                .assertThat().body("lastName", equalTo("Kowalski"))
+                .assertThat().body("email", equalTo("krzysztof@test.com"))
+                .assertThat().body("password", equalTo("password"))
+                .assertThat().body("phone", equalTo("+123456789"))
+                .assertThat().body("userStatus", equalTo(123))
+                .assertThat().statusCode(200);
     }
+
 }
