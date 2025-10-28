ae46a15ee7781399afbb18d05751c7ff65e89903
Bartek Dudek <bartlomiej.dudek94@gmail.com>
2025-10-28 13:55:22 +0100

    F.I.R.S.T - asercje

--- restassured-framework/src/test/java/pl/javastart/restassured/tests/pet/CreatePetTests.java
+++ restassured-framework/src/test/java/pl/javastart/restassured/tests/pet/CreatePetTests.java
@@ -4,6 +4,7 @@ import io.restassured.RestAssured;
 import io.restassured.filter.log.RequestLoggingFilter;
 import io.restassured.filter.log.ResponseLoggingFilter;
 import org.aeonbits.owner.ConfigFactory;
+import org.assertj.core.api.Assertions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -22,29 +23,32 @@ import static org.testng.Assert.assertEquals;
 
 public class CreatePetTests extends SuiteTestBase {
 
-    private Pet pet;
+    private Pet actualPet;
 
     @Test
     public void givenPetWhenPostPetThenPetIsCreatedTest() {
 
-        pet = new PetTestDataGenerator().generatePet();
+        Pet pet = new PetTestDataGenerator().generatePet();
 
-        Pet actualPet = given().body(pet).contentType("application/json")
+        actualPet = given().body(pet).contentType("application/json")
                 .when().post("pet")
                 .then().statusCode(200).extract().as(Pet.class);
 
-        assertEquals(actualPet.getId(), pet.getId(), "Pet id");
-        assertEquals(actualPet.getName(), pet.getName(), "Pet name");
+        pet.setName("Diego");
+        Assertions.assertThat(actualPet).describedAs("Send Pet was different than received by API").usingRecursiveComparison().isEqualTo(pet);
     }
 
     @AfterMethod
     public void cleanUpAfterTest() {
         ApiResponse apiResponse = given().contentType("application/json")
-                .when().delete("pet/{petId}", pet.getId())
+                .when().delete("pet/{petId}", actualPet.getId())
                 .then().statusCode(200).extract().as(ApiResponse.class);
 
-        assertEquals(apiResponse.getCode(), Integer.valueOf(200), "Code");
-        assertEquals(apiResponse.getType(), "unknown", "Type");
-        assertEquals(apiResponse.getMessage(), pet.getId().toString(), "Pet id");
+        ApiResponse expectedApiResponse = new ApiResponse();
+        expectedApiResponse.setCode(200);
+        expectedApiResponse.setType("unknown");
+        expectedApiResponse.setMessage(actualPet.getId().toString());
+
+        Assertions.assertThat(apiResponse).describedAs("API Response from system was not as expected").usingRecursiveComparison().isEqualTo(expectedApiResponse);
     }
 }
